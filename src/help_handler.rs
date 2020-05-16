e49ed10fa913d9010e481f52217dd5f3389c0ce1
Cory Forsstrom <cforsstrom18@gmail.com>
2020-05-15 17:45:47 -0700

    add markdown support

--- src/help_handler.rs
+++ src/help_handler.rs
@@ -1,5 +1,11 @@
 use crate::event::Event;
+
+use std::path::Path;
 use std::{collections::HashMap, sync::mpsc::Sender};
+
+use mdcat::{ResourceAccess, Settings, TerminalCapabilities, TerminalSize};
+use pulldown_cmark::{Options, Parser};
+use syntect::parsing::SyntaxSet;
 
 pub struct HelpHandler {
     writer: Sender<Event>,
@@ -14,9 +20,24 @@ impl HelpHandler {
 
     pub fn show_help(&self, file: &str) {
         if self.files.contains_key(file) {
-            self.writer
-                .send(Event::MudOutput(self.files[file].to_string()))
-                .unwrap();
+            let mut md_bytes = vec![];
+
+            let file = self.files[file];
+
+            let mut options = Options::empty();
+            options.insert(Options::ENABLE_TASKLISTS);
+            options.insert(Options::ENABLE_STRIKETHROUGH);
+
+            let parser = Parser::new_ext(&file, options);
+
+            // Useless as files are embedded into binary.
+            let base_dir = Path::new("resources/help/");
+
+            let _ = mdcat::push_tty(&md_settings(), &mut md_bytes, &base_dir, parser);
+
+            let md_string = String::from_utf8(md_bytes).unwrap();
+
+            self.writer.send(Event::MudOutput(md_string)).unwrap();
         } else {
             self.writer
                 .send(Event::Info("No such helpfile found".to_string()))
@@ -31,4 +52,15 @@ fn load_files() -> HashMap<&'static str, &'static str> {
     files.insert("welcome", include_str!("../resources/help/welcome.md"));
     files.insert("scripting", include_str!("../resources/help/scripting.md"));
     files
+}
+
+fn md_settings() -> Settings {
+    let terminal_size = TerminalSize::detect().unwrap_or_default();
+
+    Settings {
+        terminal_capabilities: TerminalCapabilities::ansi(),
+        terminal_size,
+        resource_access: ResourceAccess::LocalOnly,
+        syntax_set: SyntaxSet::load_defaults_newlines(),
+    }
 }

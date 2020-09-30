6d9fa7aaff306096515beb4d619d3b3819e58ad9
Linus Probert <linus.probert@gmail.com>
2020-09-30 13:12:46 +0200

    Patches a connection crash caused by new TLS support

--- src/event.rs
+++ src/event.rs
@@ -131,7 +131,7 @@ impl EventHandler {
             }
             Event::Connect(Connection { host, port, tls }) => {
                 self.session.disconnect();
-                if self.session.connect(&host, port, tls.unwrap()) {
+                if self.session.connect(&host, port, tls.unwrap_or_default()) {
                     let (writer, reader): (Sender<TelnetData>, Receiver<TelnetData>) = channel();
                     spawn_receive_thread(self.session.clone());
                     spawn_transmit_thread(self.session.clone(), reader);

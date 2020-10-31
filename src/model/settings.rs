359e5d5fa1d38723d3130cf60318110d3a7f41a2
chris west <c@xvxx.io>
2020-10-31 13:20:28 -0700

    Allows new settings (#169)
    * Adds check for known settings
    
    * Adds basic settings test

--- src/model/settings.rs
+++ src/model/settings.rs
@@ -11,27 +11,23 @@ pub struct Settings {
 
 pub const LOGGING_ENABLED: &str = "logging_enabled";
 pub const TTS_ENABLED: &str = "tts_enabled";
+pub const SETTINGS: [&str; 2] = [LOGGING_ENABLED, TTS_ENABLED];
 
 impl Settings {
     pub fn get(&self, key: &str) -> Result<bool> {
-        if let Some(value) = self.settings.get(key) {
-            Ok(*value)
+        if SETTINGS.contains(&key) {
+            Ok(*self.settings.get(key).unwrap_or(&false))
         } else {
             bail!("Unknown setting: {}", key)
         }
     }
 
     pub fn set(&mut self, key: &str, value: bool) -> Result<()> {
-        match key {
-            LOGGING_ENABLED => {
-                self.settings.insert(key.to_string(), value);
-                Ok(())
-            }
-            TTS_ENABLED => {
-                self.settings.insert(key.to_string(), value);
-                Ok(())
-            }
-            _ => bail!("Unknown setting: {}", key),
+        if SETTINGS.contains(&key) {
+            self.settings.insert(key.to_string(), value);
+            Ok(())
+        } else {
+            bail!("Unknown setting: {}", key)
         }
     }
 }
@@ -48,5 +44,51 @@ impl Default for Settings {
 impl SaveData for Settings {
     fn relative_path() -> std::path::PathBuf {
         crate::DATA_DIR.join("config").join("settings.ron")
+    }
+}
+
+#[cfg(test)]
+mod settings_test {
+    use super::*;
+
+    #[test]
+    fn get_settings() {
+        let settings = Settings::default();
+
+        assert_eq!(true, settings.get(TTS_ENABLED).unwrap());
+        assert_eq!(false, settings.get(LOGGING_ENABLED).unwrap());
+        assert_eq!(
+            "Unknown setting: SOMETHING_RANDOM",
+            settings.get("SOMETHING_RANDOM").unwrap_err().to_string()
+        );
+    }
+
+    #[test]
+    fn new_settings() {
+        let map = HashMap::new();
+        let settings = Settings { settings: map };
+        assert_eq!(false, settings.get(TTS_ENABLED).unwrap());
+        assert_eq!(
+            "Unknown setting: SOMETHING_RANDOM",
+            settings.get("SOMETHING_RANDOM").unwrap_err().to_string()
+        );
+    }
+
+    #[test]
+    fn set_settings() {
+        let mut settings = Settings::default();
+
+        settings.set(TTS_ENABLED, false).unwrap();
+        settings.set(LOGGING_ENABLED, true).unwrap();
+
+        assert_eq!(false, settings.get(TTS_ENABLED).unwrap());
+        assert_eq!(true, settings.get(LOGGING_ENABLED).unwrap());
+        assert_eq!(
+            "Unknown setting: SOMETHING_RANDOM",
+            settings
+                .set("SOMETHING_RANDOM", true)
+                .unwrap_err()
+                .to_string()
+        );
     }
 }

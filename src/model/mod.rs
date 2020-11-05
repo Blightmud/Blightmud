b6aa513d283d051277bfd02859dd2a90512be460
chris west <c@xvxx.io>
2020-11-05 11:45:19 -0800

    Adds ctrl-c prompt on quit

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -3,4 +3,6 @@ mod line;
 mod settings;
 pub use connection::{Connection, Servers};
 pub use line::Line;
-pub use settings::{Settings, LOGGING_ENABLED, MOUSE_ENABLED, SAVE_HISTORY, SETTINGS, TTS_ENABLED};
+pub use settings::{
+    Settings, CONFIRM_QUIT, LOGGING_ENABLED, MOUSE_ENABLED, SAVE_HISTORY, SETTINGS, TTS_ENABLED,
+};

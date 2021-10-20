548038dc23c306bed7b99607e32dfae534b6fade
Linus Probert <linus.probert@gmail.com>
2021-10-20 13:26:38 +0200

    Adds documentation

--- src/ui/help_handler.rs
+++ src/ui/help_handler.rs
@@ -201,6 +201,7 @@ fn load_files() -> HashMap<&'static str, &'static str> {
         "regex" => "regex.md",
         "line" => "line.md",
         "mud" => "mud.md",
+        "fs" => "fs.md",
         "audio" => "audio.md",
         "log" => "log.md",
         "config_scripts" => "config_scripts.md",

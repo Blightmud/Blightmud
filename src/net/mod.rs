1da1bfd4eeded5acd29d0d6496173b0b16d48e45
Daniel McCarney <daniel@binaryparadox.net>
2023-04-01 11:28:04 -0400

    net: replace native-tls with rustls.
    This commit replaces the dependency used for TLS MUD connections. The
    `native-tls` crate is replaced with `rustls`. Rustls has the advantage
    of being a pure Rust implementation of TLS 1.2 and TLS 1.3, requiring no
    native code except in the underlying *ring* dependency that provides
    cryptographic primitives.
    
    Since we're no longer delegating to the system specific TLS
    implementation via `native-tls` we must provide our own root certificate
    store. For this purpose we use the `webpki-roots` crate. It provides
    a reasonable set of default root certificates that closely follows
    Mozilla's curated root program.
    
    Blightmud still requires OpenSSL after this commit due to the `git2`
    dependency, but it is no longer used when communicating to MUD servers.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -5,6 +5,7 @@ pub use self::{
     rw_stream::RwStream,
     tcp_stream::{spawn_connect_thread, spawn_receive_thread, spawn_transmit_thread, BUFFER_SIZE},
     telnet::{TelnetHandler, TelnetMode},
+    tls::CertificateValidation,
     util::open_tcp_stream,
 };
 
@@ -14,4 +15,5 @@ mod output_buffer;
 mod rw_stream;
 mod tcp_stream;
 mod telnet;
+mod tls;
 mod util;
---
+++ tests/certs/README.md
@@ -0,0 +1,8 @@
+# Test TLS Certificates
+
+To regenerate:
+
+* Install [minica]
+* `minica --domains 'localhost'`
+
+[minica]: https://github.com/jsha/minica

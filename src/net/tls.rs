99e37a06c1a150e1311eced7c52ac2f58455bbb0
lprobert <linus.probert@volvocars.com>
2023-08-10 10:33:27 +0200

    Updates strip-ansi-escapes to 0.2.0
    Also fixed a clippy warning about a deprecated function

--- src/net/tls.rs
+++ src/net/tls.rs
@@ -85,7 +85,7 @@ impl TlsStream {
 
     fn default_root_certs() -> RootCertStore {
         let mut root_store = RootCertStore::empty();
-        root_store.add_server_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
+        root_store.add_trust_anchors(webpki_roots::TLS_SERVER_ROOTS.iter().map(|ta| {
             OwnedTrustAnchor::from_subject_spki_name_constraints(
                 ta.subject,
                 ta.spki,
